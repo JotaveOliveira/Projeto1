4ea2fb5587da44727a792b483cbeb4b5b7173dec
leooangelo <leozinhosp321@hotmail.com>
2019-12-26 13:05:54 -0300

    Config Jpa,Controllers e Dao do Produto

--- src/main/java/com/controle/estoque/controllers/ProdutoController.java
+++ src/main/java/com/controle/estoque/controllers/ProdutoController.java
@@ -1,5 +1,57 @@
 package com.controle.estoque.controllers;
 
+import java.util.List;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.controle.estoque.dao.ProdutoDao;
+import com.controle.estoque.entity.Produto;
+
+@Controller
+@RequestMapping("/produtos")
 public class ProdutoController {
+	
+	@Autowired
+	private ProdutoDao produtoDao;
+	
+	@RequestMapping("/form")
+	public ModelAndView form(Produto produto) {
+		ModelAndView modelAndView = new ModelAndView("produtos/form");
+		return modelAndView;
+		
+	}
+	
+	@GetMapping
+	public ModelAndView listar() {
+		List<Produto> produtos = produtoDao.listar();
+		ModelAndView modelAndView = new ModelAndView("produtos/lista");
+		modelAndView.addObject("produtos", produtos);
+		return modelAndView;
+	}
+	
+	@PostMapping
+	public ModelAndView gravar(MultipartFile sumario, @Valid Produto produto, BindingResult result, 
+			RedirectAttributes redirectAttributes) {
+		if(result.hasErrors()) {
+			return form(produto);
+		}
+	
+		produtoDao.gravar(produto);
+		
+		redirectAttributes.addFlashAttribute("message", "Produto cadastrado com sucesso!");
+		
+		return new ModelAndView("redirect:/produtos");
+	}
+	
 
 }
--- src/main/java/com/controle/estoque/dao/ProdutoDao.java
+++ src/main/java/com/controle/estoque/dao/ProdutoDao.java
@@ -1,5 +1,31 @@
 package com.controle.estoque.dao;
 
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+import org.springframework.stereotype.Repository;
+
+import com.controle.estoque.entity.Produto;
+
+@Repository
+@Transactional
 public class ProdutoDao {
+	
+	@PersistenceContext
+	private EntityManager manager;
+	
+	public void gravar(Produto produto) {
+		manager.persist(produto);
+		
+	}
+
+	public List<Produto> listar() {
+		return manager.createQuery("Select distinct (p) from Prdutos", Produto.class)
+				.getResultList();
+		
+	}
 
 }
