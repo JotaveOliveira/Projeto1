14b815294f802a2575f9e386a85bb5f1a7d7d807
João Vitor <jv.oa202@gmail.com>
2019-12-26 12:35:56 -0300

    UsuarioDao

--- src/main/java/com/controle/estoque/dao/UsuarioDao.java
+++ src/main/java/com/controle/estoque/dao/UsuarioDao.java
@@ -1,5 +1,32 @@
 package com.controle.estoque.dao;
 
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+import com.controle.estoque.entity.Usuario;
+
 public class UsuarioDao {
 
+	@PersistenceContext
+	private EntityManager manager;
+
+	public void gravar(Usuario usuario) {
+		manager.persist(usuario);
+	}
+	public boolean ExistEmail(String email) {
+		 List<Usuario> usuarios = manager.createQuery("select u from Usuario u where u.email = :email", Usuario.class)
+	                .setParameter("email", email).getResultList();
+
+	        if (!usuarios.isEmpty()) {
+	            return true;
+	        } else
+	            return false;
+	}
+	@Transactional
+	public void update(Usuario usuario ) {
+		 manager.merge(usuario);
+	}
 }
